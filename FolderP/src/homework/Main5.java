259082cc5198d3fab202a1a938e04949fdffaa3f
Yaroslav Pryshliak <yaroslavpryshliak09@gmail.com>
2017-05-31 15:37:10 +0300

    coment

--- FolderP/src/homework/Main5.java
+++ FolderP/src/homework/Main5.java
@@ -8,7 +8,7 @@ public class Main5 {
 	private static String comto;
 
 	public static void main(String[] args) {
-	    System.out.println(comps(comto));
+	    System.out.println(comps(comto));  //vuklukaje metod "comps"(na "private static String comto;")
 }
 	 static int comps(String comto2){
 			sc = new Scanner(System.in);
@@ -21,16 +21,15 @@ public class Main5 {
 	        int min = Math.min(len1, len2); // znahodumo minimum dlja vidwtovhyvnja
 	        char[] m1 = s1.toCharArray(); // peretvoryjem v masuv chariv m1
 	        char[] m2 = s2.toCharArray(); // peretvoryjem v masuv chariv m2
-	        {int a = 0;
+	        int a = 0;
 	        while (a < min) { 
 	            char c1 = m1[a]; //masuv chary text (po zukly)
 	            char c2 = m2[a];
 	            if (c1 != c2) {
-	                return c1 - c2; // jakwo zminutu minysu zminutsa porjadok minysy perwogo (vertaje robot nad charamu)
+	                return c1 - c2; // jakwo zminutu minysu zminutsa porjadok minysy perwogo (vertaje roboty nad charamu)
 	            }
 	            ++a;
 	        }
 			return len1-len2; // jakwo zminutu minysu (zminutsa porjadok rozmiwenja vid original compareTo (vertaje roboty nad int))
-			}  
 		}
 }
